2344799e41076dcb016e361821159be068bba717
wardaddyov <amir.sangrudi@outlook.com>
2025-12-15 05:54:46 +0330

    initial

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+
+  site: "https://eementrade.com",
+  integrations: [tailwind(), mdx(), sitemap(), icon(), react()],
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "IRANYekanBakh",
+          // "IranYekan",
+          // //"IranSans",
+          // "Bricolage Grotesque Variable",
+          // "Inter Variable",
+          // "Inter",
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
